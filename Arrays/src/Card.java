9a6cc73dfcc60002eb255195c137b1ad1a92ec68
Falio <f.fremouw@gmail.com>
2014-12-09 19:42:09 +0100

    card class geupdate

--- Arrays/src/Card.java
+++ Arrays/src/Card.java
@@ -1,28 +1,22 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by Fernflower decompiler)
+//
+
 public class Card implements Comparable<Card> {
+	Number number;
+	Suit suit;
 
-
-	/**
-	 * Constructor
-	 * 
-	 * @param number
-	 * @param suit
-	 */
 	Card(Number num, Suit suit) {
+		this.number = num;
+		this.suit = suit;
 	}
 
-
-	/**
-	 * Pretty-print deze Card als string
-	 */
 	public String toString() {
-		return "Aas" + " van " + "Ruiten";
+		return this.number + " van " + this.suit;
 	}
 
-	/**
-	 * Vergelijk twee kaarten.
-	 */
 	public int compareTo(Card card) {
-		return 0;
+		return card.number == this.number && card.suit == this.suit?0:-1;
 	}
-
 }
