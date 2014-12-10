71188c8db4ccde6d53be2a4fc84cd1aa09971579
Falio <f.fremouw@gmail.com>
2014-12-10 11:11:41 +0100

    unit test geuncommit

--- Arrays/src/test/java/CardTest1.java
+++ Arrays/src/test/java/CardTest1.java
@@ -9,9 +9,9 @@ public class CardTest1 {
         Card c1 = new Card(Number.AAS, Suit.HARTEN);
         Card c2 = new Card(Number.TWEE, Suit.SCHOPPEN);
 
-        //assertTrue(c1.compareTo(c1) == 0);
-        //assertTrue(c1.compareTo(c2) > 0);
-        //assertTrue(c2.compareTo(c1) < 0);
+        assertTrue(c1.compareTo(c1) == 0);
+        assertTrue(c1.compareTo(c2) > 0);
+        assertTrue(c2.compareTo(c1) < 0);
     }
 
 }
